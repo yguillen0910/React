6198038bedc5409ce3c8940ab2bcc6d5a01e421a
Yuzmhar Guillen <yuzmhar.guillen@elemetrica.com>
2019-02-21 00:53:59 -0300

    Sections 1,2,3 y 4

---
+++ jsx/src/index.js
@@ -0,0 +1,25 @@
+//Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+function getButtonText() {
+  return "Click on me!";
+}
+
+//Create react component
+const App = () => {
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+//Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
